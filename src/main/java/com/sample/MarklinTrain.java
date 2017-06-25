6a8315fcf6c7538dd66475fd3705c84005fd9f1e
Miguel Dias <m3k3r1@gmail.com>
2017-06-25 18:53:58 +0100

    Components groups

--- src/main/java/com/sample/MarklinTrain.java
+++ src/main/java/com/sample/MarklinTrain.java
@@ -5,6 +5,13 @@ import java.util.Observable;
 public class MarklinTrain extends Observable implements Constants{
 	Integer componentes[];
 	int nModel;
+	
+	int group[][] = {{0,1,2,3},
+			{4,5,6,7,8,9,10},
+			{11,12,13,14,15,16,17,18},
+			{19,20,21,22,23,24},
+			{25,26,27,28,29,30,31,32} 
+			};
 
 	public MarklinTrain(){
 		componentes = new Integer[TRAIN_COMPONENTS_SIZE];
