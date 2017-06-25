503edffbeb94a8f9f464c73349974a15a88948b2
Vascoide <vascoide650@hotmail.com>
2017-06-25 03:05:03 +0100

    Contants Fixes Messages

--- src/main/java/com/sample/Constants.java
+++ src/main/java/com/sample/Constants.java
@@ -1,6 +1,38 @@
-package com.sample;
+package marklinTrain;
 
 interface Constants {
 	static final int TRAIN_COMPONENTS_SIZE = 20;
-	static final String FIX_1 = "ola";
+	static final String FIX_0 = "Ajuste o acoplador com a ajuda do indicador de acoplamento 7001 ou substítuir o acoplador (ver capítulo 7.3 para mais detalhes).";
+	static final String FIX_1 = "A fonte deste problema poderá ser alcançada através da localização de uma queimadura no interior do corpo.";
+	static final String FIX_2 = "A fonte deste problema poderá ser alcançada através da localização de uma queimadura no interior do corpo.";
+	static final String FIX_3 = "Em locomotivas com várias luzes principais, é necessário tocar no ponto de soldadura, um de cada vez, para determinar qual das luzes princiais causa o curto-circuito.";
+	static final String FIX_4 = "Repor a posição alavanca seletora se necessário.";
+	static final String FIX_5 = "Prender a placa de comutação por baixo da alavanca.";
+	static final String FIX_6 = "Dobrar a placa de comutação para produzir contacto mais fiável.";
+	static final String FIX_7 = "Trocar o patim da locomotiva.";
+	static final String FIX_8 = "Resoldar o fio.";
+	static final String FIX_9 = "Resoldar o fio.";
+	static final String FIX_10 = "Levar a locomotiva a um ponto de venda para trocar a válvula.";
+	static final String FIX_11 = "As mudanças sofreram dano e deve-se levar a locomotiva a um ponto de venda.";
+	static final String FIX_12 = "Repor as escovas.";
+	static final String FIX_13 = "Repor ou voltar a soldar o(s) fio(s).";
+	static final String FIX_14 = "A mola de corredor da unidade reversa tem de ser ajustada.";
+	static final String FIX_15 = "Pressionar a mola de tensão para baixo com uma pinça até soltar o interruptor.";
+	static final String FIX_16 = "Dobrar cuidadosamente a ponte ligeiramente para cima e substítuir o interruptor (nº 22683) se necessário";
+	static final String FIX_17 = "Usando uma chave de fendas isolada, cuidadosamente levantar um dos dedos para passar corrente (ver secção 9.1.2 caso a locomotiva funcionar depois deste passo).";
+	static final String FIX_18 = "Usando uma chave de fendas isolada, pressionar com cuidado e um de cada vez, os pontos de contacto contra os 'switching drums'. Se a locomotiva funcionar agora, tanto num caso como no anterior, então os pontos de contacto não têm tensão suficiente.";
+	static final String FIX_19 = "Verificar as conexões no sistema da catenária.";
+	static final String FIX_20 = "Reiniciar a alavanca se necessário.";
+	static final String FIX_21 = "Ver secção 9.2.2 b/c para instruções de reparação.";
+	static final String FIX_22 = "Dobrar a mola para cima.";
+	static final String FIX_23 = "Repor as molas (ver capítulo 7.8).";
+	static final String FIX_24 = "Report o pantógrafo (ver capítulo 7.8)";
+	static final String FIX_25 = "Voltar a soldar os fios.";
+	static final String FIX_26 = "Ajustar o 'rocker stroke' ou report a unidade reversa.";
+	static final String FIX_27 = "Encaixar de novo o corredor.";
+	static final String FIX_28 = "Usando um alicate, dobrar cuidadosamente o pin para baixo de modo a ficar perpendicular.";
+	static final String FIX_29 = "Endireitar o gancho com um alicate.";
+	static final String FIX_30 = "Reencaminhar o fio de modo a afastá-lo do interruptor.";
+	static final String FIX_31 = "Dobrar cuidadosamente a placa de soldadura de contacto para baixo e ajustar o 'rocker stroke'.";
+	static final String FIX_32 = "Usando uma chave de fendas isolada, cuidadosamente pressiona com o ponto de contacto para a direção defeituosa de viagem contra o interruptor. Se a locomotiva funciona agora, ver a secção 9.3.5 para os procedimentos de reparação neste capítulo.";
 }
